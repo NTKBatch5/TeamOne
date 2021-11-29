7ca347b0c57bd4a2935776add03a8f74b92308fb
aghaz <aghaz@LAPTOP-L2PTAS43.myfiosgateway.com>
2021-11-29 18:52:51 -0500

    initial commit...

--- src/one/TeamOne.java
+++ src/one/TeamOne.java
@@ -3,6 +3,6 @@ package one;
 public class TeamOne {
 
 	public static void main(String[] args) {
-		System.out.println("Team One Initial code...");
+		System.out.println("Team One Initial code....");
 	}
 }
