c09d1585d8c59bed25b268e356643b89860af796
Wahida <Wahida@Wahida-pc.fios-router.home>
2021-11-30 07:11:14 +0430

    commit ....

--- src/one/TeamOne.java
+++ src/one/TeamOne.java
@@ -14,6 +14,7 @@ System.out.println("one more try");
 		System.out.println("any conflict yet?");
 		
 		System.out.println("we got conflicts yesterday, where are they for today?");
+		System.out.println(" mine gets delete?");
 
 
  
